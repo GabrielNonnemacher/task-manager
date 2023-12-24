717f330bd7da826390733f68336365a7da6f6037
Gabriel Nonnemacher <nonnemacher99gabriel@gmail.com>
2023-12-24 09:42:20 -0300

    Task #9 - estilizando delete-button e edit-button

--- src/app/components/delete-button/delete-button.component.ts
+++ src/app/components/delete-button/delete-button.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-delete-button',
+  selector: 'delete-button',
   templateUrl: './delete-button.component.html',
   styleUrls: ['./delete-button.component.scss']
 })
 export class DeleteButtonComponent {
+  @Input() id!: number | undefined;
+  @Output() click = new EventEmitter;
 
+  public onClick(): void {
+    this.click.emit(this.id);
+  }
 }
--- src/app/components/edit-button/edit-button.component.ts
+++ src/app/components/edit-button/edit-button.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-edit-button',
+  selector: 'edit-button',
   templateUrl: './edit-button.component.html',
   styleUrls: ['./edit-button.component.scss']
 })
 export class EditButtonComponent {
+  @Input() id!: number | undefined;
+  @Output() click = new EventEmitter;
 
+  public onClick(): void {
+    this.click.emit(this.id);
+  }
 }
